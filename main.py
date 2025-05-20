8bb72756c08f1ccde5256ea6cc6cbed0db8c2fa6
vadhiya06 <vadhiyarajesh06@gmail.com>
2025-05-21 00:35:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ bot = Client(
     bot_token=BOT_TOKEN
 )
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '5885218829').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 CHANNEL_OWNERS = {}
 CHANNELS = os.environ.get('CHANNELS', '').split(',')
